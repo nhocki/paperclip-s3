1cce679b9e2855b624fecd05f6e1bd3f892d578e
Nicolás Hock Isaza <nhocki@gmail.com>
2011-12-21 22:57:40 -0500

    Adding S3_PROTOCOL option
    Fix #4

--- lib/paperclip-s3.rb
+++ lib/paperclip-s3.rb
@@ -6,7 +6,7 @@ require 'paperclip-s3/railtie'
 module Paperclip
   module S3
     
-    # Use Paperclip::S3::Glue to keep the original gems style
+    # Use Paperclip::S3::Glue to keep the original gem style
     module Glue
       def self.included base
         base.extend ClassMethods
@@ -17,9 +17,11 @@ module Paperclip
       # Extends the paperclips has_attached_file method
       # It will use S3 Storage. The credentials will be read from the environment
       def has_attached_file(name, options = {})
+        s3_protocol = !!ENV["S3_PROTOCOL"] ? ENV["S3_PROTOCOL"] : "http"
         options[:storage] = :s3
         options[:path]    ||= "/:class-:attachment/:id/:style-:basename.:extension"
         options[:bucket]  ||= ENV["S3_BUCKET"]
+        options[:s3_protocol] ||= s3_protocol
         options[:s3_credentials] ||= {
                                         :access_key_id => ENV['S3_KEY'],
                                         :secret_access_key => ENV['S3_SECRET']
