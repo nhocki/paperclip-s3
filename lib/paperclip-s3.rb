1d8cbc1479c46741d0e9482ff148764488fd55b8
Nicolás Hock Isaza <nhocki@gmail.com>
2013-02-01 11:11:30 -0500

    License & readme words

--- lib/paperclip-s3.rb
+++ lib/paperclip-s3.rb
@@ -5,14 +5,14 @@ require 'paperclip-s3/railtie'
 
 module Paperclip
   module S3
-    
+
     # Use Paperclip::S3::Glue to keep the original gem style
     module Glue
       def self.included base
         base.extend ClassMethods
       end
     end
-        
+
     module ClassMethods
       # Extends the paperclips has_attached_file method
       # It will use S3 Storage. The credentials will be read from the environment
@@ -23,9 +23,9 @@ module Paperclip
         options[:bucket]  ||= ENV["S3_BUCKET"]
         options[:s3_protocol] ||= s3_protocol
         options[:s3_credentials] ||= {
-                                        :access_key_id => ENV['S3_KEY'],
-                                        :secret_access_key => ENV['S3_SECRET']
-                                    }
+          :access_key_id => ENV['S3_KEY'],
+          :secret_access_key => ENV['S3_SECRET']
+        }
         super(name, options)
       end
     end
